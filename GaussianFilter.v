34409e19ef41233f18ffeba5e277c70bca9d153a
Trương Nhật Hoàng <162524291+wenxizhabg@users.noreply.github.com>
2026-03-22 18:02:22 +0700

    Enhance GaussianFilter with new frame signals
    Added new input and output signals for frame processing.

--- GaussianFilter.v
+++ GaussianFilter.v
@@ -19,9 +19,9 @@
 //`timescale <time_units> / <precision>
 module GaussianFilter(
     input wire pixelClock, 
-    input wire reset,                                 // Reset đồng bộ (Tích cực mức cao)
     
     // --- Giao tiếp nhận ma trận 3x3 từ RowBuffering ---
+    input wire newFrameIsPrepareI,                    // Cờ báo chuẩn bị ảnh mới                          
     input wire rowIsProcessI,                         // Cờ báo đang xử lý hàng (từ module trước)
     input wire inputIsValid,                          // Cờ báo ma trận 3x3 ngõ vào là hợp lệ
     // 9 điểm ảnh của cửa sổ trượt 3x3
@@ -30,6 +30,7 @@ module GaussianFilter(
     input wire [7:0] p31, p32, p33,
     
     // --- Giao tiếp ngõ ra đã làm mịn ---
+    output reg newFrameIsPrepareO,
     output reg rowIsProcessO,                        // Đẩy cờ trạng thái hàng đi tiếp
     output reg outputIsValid,                         // Cờ báo điểm ảnh ngõ ra hợp lệ
     output reg [7:0] valueOfOutputPixel              // Điểm ảnh đã lọc Gauss
@@ -51,7 +52,9 @@ module GaussianFilter(
 
     // 2. CONTROL UNIT & OUTPUT REGISTER: Mạch tuần tự chốt dữ liệu an toàn
     always @(posedge pixelClock) begin
-        if (reset == 1'b1) begin
+        newFrameIsPrepareO <= newFrameIsPrepareI;
+        
+        if (newFrameIsPrepareI == 1'b1) begin
             valueOfOutputPixel <= 8'b0;
             outputIsValid <= 1'b0;
             rowIsProcessO <= 1'b0;
